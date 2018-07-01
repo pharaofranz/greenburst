96bd000328fcedff82b111148231005b9b848ce7
Devansh Agarwal <devanshkv@gmail.com>
2018-06-30 23:00:20 -0400

    Redis Querying

--- dev_trunk/pointing_dump.py
+++ dev_trunk/pointing_dump.py
@@ -8,9 +8,11 @@ __email__  = 'da0017@mix.wvu.edu'
 import redis
 from datetime import datetime, timedelta
 import time
+import sys
 import numpy as np
 from apscheduler.schedulers.background import BackgroundScheduler
 import json
+import os
 
 r = redis.StrictRedis(
     host='serendip6',
@@ -34,13 +36,13 @@ if __name__ == '__main__':
     scheduler = BackgroundScheduler()
     thelist=[]
     # dump every second
-    scheduler.add_job(tick, 'interval', seconds=1,start_date=datetime.now(),end_date=datetime.now()+timedelta(seconds=10),args=[thelist])
+    scheduler.add_job(tick, 'interval', seconds=1,start_date=datetime.now(),end_date=datetime.now()+timedelta(seconds=337),args=[thelist])
     scheduler.start()
 
     try:
         # This is here to simulate application activity (which keeps the main thread alive).
         while True:
-            time.sleep(300) # do this for 5 min then break
+            time.sleep(337) # do this for 5 min then break
             break
     except (KeyboardInterrupt, SystemExit):
         # Not strictly necessary if daemonic mode is enabled but should be done if possible
@@ -52,5 +54,6 @@ if __name__ == '__main__':
             names[4]:[element[4] for element in thelist],\
             names[5]:[float(element[5]) for element in thelist],\
             names[6]:[float(element[6]) for element in thelist]}
-    with open("file.json",'w') as f:
+    outname=str(sys.argv[1])+'.json'
+    with open(outname,'w') as f:
         json.dump(data_dict,f,indent=4)
