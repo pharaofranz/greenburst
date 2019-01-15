56abac703aa0372f696fef27ba7d73bfc2fd2ee6
Devansh Agarwal <devanshkv@gmail.com>
2019-01-15 12:22:57 -0500

    pika integration

--- dev_trunk/stage_1.py
+++ dev_trunk/stage_1.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/env python3.6
 
 from subprocess import PIPE, Popen
 import filutils
@@ -8,9 +7,34 @@ import logging
 from multiprocessing import Process
 import glob
 import numpy as np
+import pika
+
+
+logger = logging.getLogger()
+format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+logging.basicConfig(level=logging.INFO, format=format)
+logging.getLogger('pika').setLevel(logging.INFO)
 
 __author__='Devansh Agarwal'
 __email__ = 'da0017@mix.wvu.edu'
+
+def stage_initer(values):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    
+    channel.queue_declare(queue='stage01_queue', durable=True)
+    
+    def callback(ch, method, properties, body):
+        values.file=body.decode()
+        logging.info(f'got it {values.file}')
+        begin_main(values)
+        ch.basic_ack(delivery_tag = method.delivery_tag)
+    
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(callback,
+                          queue='stage01_queue')
+    
+    channel.start_consuming()
 
 def mask_finder(data,chan_nos,nchan_avg,sigma):
     """
@@ -29,6 +53,7 @@ def _cmdline(command):
     """
     process = Popen(args=command,stdout=PIPE,shell=True)
     output=process.communicate()[0]
+    logging.info(f'Processed {command}')
     return output
 
 def write_and_plot(mask,chan_nos,freqs,bandpass,outdir):
@@ -57,20 +82,12 @@ def write_and_plot(mask,chan_nos,freqs,bandpass,outdir):
 
 
 
-if __name__=='__main__':
-
-    parser=ArgumentParser(description='Stage 1: Get RFI Flags, run heimdall', formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='Be verbose')
-    parser.add_argument('-n', '--nchans', type=int, help='no. of chans to calc. median over', default=64)
-    parser.add_argument('-s', '--sigma', type=int, help='sigma over which values are tagged as RFI', default=3)
-    parser.add_argument(dest='file')
-    parser.set_defaults(verbose=False)
-    values = parser.parse_args()
-
+def begin_main(values):
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     if values.verbose:
-        logging.basicConfig(level=logging.DEBUG)
+        logging.basicConfig(level=logging.DEBUG, format=format)
     else:
-        logging.basicConfig(level=logging.INFO)
+        logging.basicConfig(level=logging.INFO, format=format)
 
     filterbank=glob.glob(values.file)[0]
     
@@ -90,11 +107,39 @@ if __name__=='__main__':
     filterbank_name=filterbank.split('/')[-1].split('.')[0]
     out_dir='/ldata/trunk/{}/'.format(filterbank_name)
     _cmdline('mkdir -p {}'.format(out_dir))
+    _cmdline(f'mv {filterbank} {out_dir}/')
+    new_fil_path=f'{out_dir}{filterbank_name}.fil'
 
-    heimdall_command='tsp heimdall -dm 2 10000 -max_giant_rate 100 -boxcar_max 8 -cand_sep_filter 8 -cand_sep_dm_trial 20 -cand_sep_time 64 ' \
-            + ''.join(str(x)+' ' for x in out_chans) + ' -output_dir {}'.format(out_dir) + ' -f {}'.format(filterbank)
-
+    heimdall_command='heimdall -nsamps_gulp 524288 -dm 10 10000 -boxcar_max 128 -cand_sep_filter 256 -cand_sep_dm_trial 20 -cand_sep_time 64 ' \
+            + ''.join(str(x)+' ' for x in out_chans) + ' -output_dir {}'.format(out_dir) + ' -f {}'.format(new_fil_path)
+    logging.info(f'Running {heimdall_command}')
     p1 = Process(target = _cmdline,args=[heimdall_command])
     p1.start()
     p2 = Process(target = write_and_plot,args=[mask,chan_nos,freqs,bandpass,out_dir])
     p2.start()
+
+    p1.join()
+    p2.join()
+
+if __name__ == '__main__':
+    parser=ArgumentParser(description='Stage 1: Get RFI Flags, run heimdall', formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='Be verbose')
+    parser.add_argument('-d', '--daemon', dest='daemon', action='store_true', help='Run with AMQP')
+    parser.add_argument('-n', '--nchans', type=int, help='no. of chans to calc. median over', default=64)
+    parser.add_argument('-s', '--sigma', type=int, help='sigma over which values are tagged as RFI', default=3)
+    parser.add_argument('-f', '--file', type=str, help='Filterbank file')
+    parser.set_defaults(verbose=False)
+    parser.set_defaults(daemon=True)
+    values = parser.parse_args()
+
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    if values.verbose:
+        logging.basicConfig(level=logging.DEBUG, format=format)
+    else:
+        logging.basicConfig(level=logging.INFO, format=format)
+
+    if values.daemon:
+        logging.info('Running in daemon mode')
+        stage_initer(values)
+    else:
+        begin_main(values)
