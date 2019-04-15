73942ca74a80f0a07a0d25d74838937c9e2284d3
Devansh Agarwal <devanshkv@gmail.com>
2019-04-15 13:40:23 -0400

    send quick messages to slack via api

---
+++ dev_trunk/slack_send.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from slackclient import SlackClient
+import yaml
+import logging
+
+def send_msg_2_slack(msg):
+    with open("config/conf.yaml", 'r') as stream:
+        data_loaded = yaml.load(stream)
+    TOKEN = data_loaded['slack']['token']
+    
+    sc = SlackClient(TOKEN)
+    
+    response= sc.api_call(
+      "chat.postMessage",
+      channel="CB7BGC70E",
+      text=msg
+    )
+    return response
+
+
+if __name__ == "__main__":
+    logger = logging.getLogger()
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=format)
+    respose = send_msg_2_slack("Hello from Python! :tada:")
+    logging.info(f'{respose}')
